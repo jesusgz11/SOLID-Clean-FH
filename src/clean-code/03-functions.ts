531b83b348e9e091bf77f6d47ec46089ee947f56
Jesus Guzman <jesusguzman.0311@gmail.com>
2022-04-24 18:50:54 -0500

    New function

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -36,4 +36,17 @@
     console.log('Crear actor');
     return true;
   }
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }) => {
+    if (isDead) return 1500;
+
+    if (isSeparated) return 2500;
+
+    if (isRetired) return 3000;
+
+    return 4000;
+  };
 })();
